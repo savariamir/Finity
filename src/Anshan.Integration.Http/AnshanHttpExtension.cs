357256aa2b2213a43c51704983990b24747c9124
Amir Savari <amir.svr@gmail.com>
2021-09-13 17:30:59 +0430

    refactor AnshanHttpExtension

---
+++ src/Anshan.Integration.Http/AnshanHttpExtension.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Net.Http;
+using Anshan.Integration.Http.Caching;
+using Anshan.Integration.Http.CircuitBreaker;
+using Anshan.Integration.Http.Clock;
+using Anshan.Integration.Http.Configuration;
+using Anshan.Integration.Http.Retry.Configurations;
+using Anshan.Integration.Http.Retry.Internals;
+using EasyPipe.Extensions.MicrosoftDependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Anshan.Integration.Http
+{
+    public static class AnshanHttpExtension
+    {
+        public static IHttpClientBuilder AddAnshanHttpClient(this IServiceCollection services, string name,
+                                                             Action<HttpClient> configureClient)
+        {
+            services.AddPipeline<AnshanHttpRequestMessage, HttpResponseMessage>()
+                    .WithMiddleware<MemoryCacheMiddleware>()
+                    .WithMiddleware<RetryMiddleware>()
+                    .WithMiddleware<CircuitBreakerMiddleware>()
+                    .WithMiddleware<HttpMiddleware>();
+
+
+            var builder = services.AddHttpClient(name, configureClient);
+
+            builder.AddHttpMessageHandler((sp) => new DefaultDelegationHandler(name, sp));
+
+            builder.AddHttpMessageHandler<DefaultDelegationHandler>();
+            builder.Services.AddMemoryCache();
+
+            return builder;
+        }
+
+        public static IHttpClientBuilder AddAnshanHttpClient(this IServiceCollection services)
+        {
+            services.AddPipeline<AnshanHttpRequestMessage, HttpResponseMessage>()
+                    .WithMiddleware<MemoryCacheMiddleware>()
+                    .WithMiddleware<RetryMiddleware>()
+                    .WithMiddleware<CircuitBreakerMiddleware>()
+                    .WithMiddleware<HttpMiddleware>();
+
+            var builder = services.AddHttpClient(string.Empty);
+
+            services.AddTransient<DefaultDelegationHandler>();
+
+            builder.AddHttpMessageHandler((sp) => new DefaultDelegationHandler(string.Empty, sp));
+
+            builder.Services.AddMemoryCache();
+
+            return builder;
+        }
+
+        public static IHttpClientBuilder AddAnshanHttpClient(this IServiceCollection services, string name)
+        {
+            services.AddPipeline<AnshanHttpRequestMessage, HttpResponseMessage>()
+                    .WithMiddleware<MemoryCacheMiddleware>()
+                    .WithMiddleware<RetryMiddleware>()
+                    .WithMiddleware<CircuitBreakerMiddleware>()
+                    .WithMiddleware<HttpMiddleware>();
+
+            var builder = services.AddHttpClient(name);
+
+            builder.AddHttpMessageHandler((sp) => new DefaultDelegationHandler(name, sp));
+
+            builder.Services.AddMemoryCache();
+
+            return builder;
+        }
+
+        public static IHttpClientBuilder AddRetry(this IHttpClientBuilder builder,
+                                                  Action<RetryConfigure> retryConfigure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (retryConfigure == null)
+            {
+                throw new ArgumentNullException(nameof(retryConfigure));
+            }
+
+
+            builder.Services.AddTransient<IClock, SystemClock>();
+            builder.Services.AddMemoryCache();
+
+            builder.ConfigureHttpClient(retryConfigure);
+
+            builder.Services.Configure<AnshanFactoryOptions>(builder.Name,
+                factoryOptions => factoryOptions.IsRetryEnabled = true);
+
+            return builder;
+        }
+
+
+        public static IHttpClientBuilder AddCache(this IHttpClientBuilder builder,
+                                                  Action<CacheConfigure> cacheConfigure)
+        {
+            builder.Services.AddMemoryCache();
+            builder.Services.AddTransient<ICacheEngine, CacheEngine>();
+            builder.Services.Configure<AnshanFactoryOptions>(builder.Name, options => options.IsCacheEnabled = true);
+            builder.Services.Configure(cacheConfigure);
+            return builder;
+        }
+
+        public static IHttpClientBuilder AddCircuitBreaker(this IHttpClientBuilder builder)
+        {
+            builder.Services.AddTransient<ICircuitBreakerEngine, CircuitBreakerEngine>();
+            builder.Services.Configure<AnshanFactoryOptions>(builder.Name,
+                options => options.IsCircuitBreakerEnabled = true);
+            return builder;
+        }
+
+
+        private static IHttpClientBuilder ConfigureHttpClient(this IHttpClientBuilder builder,
+                                                              Action<RetryConfigure> retryConfigure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (retryConfigure == null)
+            {
+                throw new ArgumentNullException(nameof(retryConfigure));
+            }
+
+            builder.Services.Configure(builder.Name, retryConfigure);
+
+
+            return builder;
+        }
+    }
+}
